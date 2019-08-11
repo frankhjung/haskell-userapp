734b3fcb09378e95ca31ac7a6e616fcd2b30f9b7
Frank Jung <frankhjung@linux.com>
2019-08-11 20:29:19 +1000

    sum types

--- src/Lib.hs
+++ src/Lib.hs
@@ -41,15 +41,11 @@ makeUser :: Username -> Password -> Validation Error User
 makeUser username password =
   User <$> parseUsername username <*> parsePassword password
 
-cleanWhitespace :: Rule Text
-cleanWhitespace "" = Failure (Error ["Value cannot be empty"])
-cleanWhitespace as = Success (T.filter (/= ' ') (T.strip as))
-
 checkLength :: Int -> Rule Text
 checkLength maxlength as =
-  if T.length as <= maxlength
-    then Success as
-    else Failure (Error ["Value too long, can not exceed " <> (T.pack . show) maxlength])
+  if T.length as > maxlength
+    then Failure (Error ["Value too long, can not exceed " <> (T.pack . show) maxlength])
+    else Success as
 
 checkPasswordLength :: Rule Password
 checkPasswordLength = coerce (checkLength 15) :: Rule Password
@@ -57,23 +53,27 @@ checkPasswordLength = coerce (checkLength 15) :: Rule Password
 checkUsernameLength :: Rule Username
 checkUsernameLength = coerce (checkLength 10) :: Rule Username
 
+cleanWhitespace :: Rule Text
+cleanWhitespace "" = Failure (Error ["Value cannot be empty"])
+cleanWhitespace as = Success (T.filter (/= ' ') (T.strip as))
+
 display :: Username -> Password -> IO ()
 display username password =
   case makeUser username password of
-    Success (User u _) -> TIO.putStrLn (T.concat ["Welcome ", coerce u])
     Failure (Error e)  -> TIO.putStr (T.unlines e)
+    Success (User u _) -> TIO.putStrLn (T.concat ["Welcome ", coerce u])
 
 parsePassword :: Rule Password
 parsePassword password =
   case validatePassword password of
-    Success p -> Success p
     Failure e -> Failure (Error ["Invalid password:"] <> e)
+    Success p -> Success p
 
 parseUsername :: Rule Username
 parseUsername username =
   case validateUsername username of
-    Success u -> Success u
     Failure e -> Failure (Error ["Invalid username:"] <> e)
+    Success u -> Success u
 
 requireAlphaNum :: Rule Text
 requireAlphaNum as =
@@ -84,11 +84,12 @@ requireAlphaNum as =
 validatePassword :: Rule Password
 validatePassword password =
   case (coerce cleanWhitespace :: Rule Password) password of
-    Success p -> (coerce requireAlphaNum :: Rule Password) p *> checkPasswordLength p *> Success p
     Failure e -> Failure e
+    Success p -> (coerce requireAlphaNum :: Rule Password) p *> checkPasswordLength p *> Success p
 
 validateUsername :: Rule Username
 validateUsername username =
   case (coerce cleanWhitespace :: Rule Username) username of
-    Success u -> (coerce requireAlphaNum :: Rule Username) u *> checkUsernameLength u *> Success u
     Failure e -> Failure e
+    Success u -> (coerce requireAlphaNum :: Rule Username) u *> checkUsernameLength u *> Success u
+
